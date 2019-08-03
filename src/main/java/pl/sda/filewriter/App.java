09c371bec62e8cd7b399cdefe2b3dcc56d754433
Krzysztof Świątek <swiatek.krzysztof95@gmail.com>
2019-08-03 15:49:49 +0200

    Added Enum Singleton

--- src/main/java/pl/sda/filewriter/App.java
+++ src/main/java/pl/sda/filewriter/App.java
@@ -1,13 +1,60 @@
 package pl.sda.filewriter;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
+    static class Task1 implements Runnable{
+        @Override
+        public void run() {
+            //FileWriterSingleton fileWriterSingleton = FileWriterSingleton.getInstance();
+            SingletonEnum fileWriterSingleton = SingletonEnum.INSTANCE;
+            System.out.println(fileWriterSingleton);
+            fileWriterSingleton.write("Blabla","blublu.txt");
+        }
+    }
+    static class Task2 implements Runnable{
+
+        @Override
+        public void run() {
+            //FileWriterSingleton fileWriterSingleton = FileWriterSingleton.getInstance();
+            SingletonEnum fileWriterSingleton = SingletonEnum.INSTANCE;
+            System.out.println(fileWriterSingleton);
+            fileWriterSingleton.write("Bleble","blublu.txt");
+
+        }
+    }
+    static class Task3 implements Runnable{
+
+        @Override
+        public void run() {
+            //FileWriterSingleton fileWriterSingleton = FileWriterSingleton.getInstance();
+            SingletonEnum fileWriterSingleton = SingletonEnum.INSTANCE;
+            System.out.println(fileWriterSingleton);
+            fileWriterSingleton.write("Bribri","blublu.txt");
+
+        }
+    }
+    static class Task4 implements Runnable{
+
+        @Override
+        public void run() {
+            //FileWriterSingleton fileWriterSingleton = FileWriterSingleton.getInstance();
+            SingletonEnum fileWriterSingleton = SingletonEnum.INSTANCE;
+            System.out.println(fileWriterSingleton);
+            fileWriterSingleton.write("Blaipblaip","blublu.txt");
+
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Thread thread1 = new Thread(new Task1());
+        Thread thread2 = new Thread(new Task2());
+        Thread thread3 = new Thread(new Task3());
+        Thread thread4 = new Thread(new Task4());
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+        FileWriterSingleton.getInstance().write("Brrrrrr","blublu.txt");
     }
 }
